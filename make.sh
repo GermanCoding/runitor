f8bde1366d1b58999bed4ff60371f44bf73c298a
GermanCoding <github@germancoding.com>
2022-10-23 22:13:49 +0200

    Fix arm architectures

--- make.sh
+++ make.sh
@@ -2,25 +2,28 @@
 set -e
 program="runitor"
 version="1.0.0"
-revision=3
+revision=4
 debversion="${version}-${revision}"
 github="https://github.com/bdd/runitor/releases/download/"
 prefix="/usr/bin"
 pversion="v$version"
 platform="linux"
 control="DEBIAN/control"
-supported_archs=("amd64"  "arm"  "arm64")
+declare -A supported_archs
+supported_archs=(["amd64"]="amd64" ["armhf"]="arm" ["armel"]="arm" ["arm64"]="arm64")
 
-for arch in ${supported_archs[*]}; do
+rm -rf debs
+for arch in ${!supported_archs[@]}; do
+     github_arch=${supported_archs[$arch]}
      package="${program}_${debversion}_${arch}"
      directory=${package}${prefix}
      binary=${directory}/${program}
      mkdir -p ${directory}
-     wget -O "${binary}" "${github}${pversion}/${program}-${pversion}-${platform}-${arch}"
+     wget -O "${binary}" "${github}${pversion}/${program}-${pversion}-${platform}-${github_arch}"
      chmod +x ${binary}
      cp -ar DEBIAN ${package}
-     sed -i -e "s/Version: TEMPLATE$/Version: $debversion/g" ${package}/${control}
-     sed -i -e "s/Architecture: TEMPLATE$/Architecture: $arch/g" ${package}/${control}
+     sed -i -e "s/Version: TEMPLATE$/Version: ${debversion}/g" ${package}/${control}
+     sed -i -e "s/Architecture: TEMPLATE$/Architecture: ${arch}/g" ${package}/${control}
      dpkg-deb --build --root-owner-group ${package}
      rm -r ${package}
 done
