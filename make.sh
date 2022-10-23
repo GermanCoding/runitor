bf6c3c7ab668cee843e47387e145601a6c99520e
GermanCoding <github@germancoding.com>
2022-10-23 17:01:02 +0200

    Fix debian revision numbers

--- make.sh
+++ make.sh
@@ -2,7 +2,8 @@
 set -e
 program="runitor"
 version="1.0.0"
-patch=1
+revision=1
+debversion="${version}-${revision}"
 github="https://github.com/bdd/runitor/releases/download/"
 prefix="/usr/bin"
 pversion="v$version"
@@ -11,10 +12,10 @@ control="DEBIAN/control"
 supported_archs=("amd64"  "arm"  "arm64")
 
 for arch in ${supported_archs[*]}; do
-     package="${program}_${version}-${patch}_${arch}"
+     package="${program}_${debversion}_${arch}"
      wget -P ${package}${prefix}/${program} ${github}${pversion}/${program}-${pversion}-${platform}-${arch}
      cp -ar DEBIAN ${package}
-     sed -i -e "s/Version: TEMPLATE$/Version: $version/g" ${package}/${control}
+     sed -i -e "s/Version: TEMPLATE$/Version: $debversion/g" ${package}/${control}
      sed -i -e "s/Architecture: TEMPLATE$/Architecture: $arch/g" ${package}/${control}
      dpkg-deb --build --root-owner-group ${package}
      rm -r ${package}
